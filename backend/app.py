27ec7617a59534f3e2efef703a9587d99fd217e9
Gabriel <gabrielzsogura@gmail.com>
2026-04-11 15:11:45 -0300

    test: ativa o debug do app.py

--- backend/app.py
+++ backend/app.py
@@ -597,4 +597,4 @@ def deletar_funcionario_db():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=True)
