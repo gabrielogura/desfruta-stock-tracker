0a406755170a64434e7885d5e4f5afd9d9a70602
Gabriel <gabrielzsogura@gmail.com>
2026-05-08 07:07:17 -0300

    chore: Informa o usuário e senha da conta demo

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -75,7 +75,7 @@ export function LoginPage({ onAuthed }) {
             type="text"
             name="user"
             id="user"
-            placeholder="demo"
+            placeholder="username: demo"
             value={user}
             onChange={(e) => setUser(e.target.value)}
             autoComplete="username"
@@ -89,7 +89,7 @@ export function LoginPage({ onAuthed }) {
               type={showPassword ? 'text' : 'password'}
               name="password"
               id="password"
-              placeholder="demo123"
+              placeholder="password: demo123"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               autoComplete="current-password"
